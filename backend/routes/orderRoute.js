dadbe5e6bd3f775838fed5f66ffd73e267519eb7
Your Name <diyanath2205200@gmail.com>
2025-04-18 16:41:20 +0530

    Order Routes files

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -2,6 +2,8 @@ import express from "express"
 import authMiddleware from "../middleware/auth.js"
 import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js"
 
+
+
 const orderRouter = express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder)
@@ -9,4 +11,6 @@ orderRouter.post("/verify",verifyOrder)
 orderRouter.post("/userorders",authMiddleware,userOrders)
 
 export default orderRouter;
+
+
 
